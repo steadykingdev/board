2113b10c22c3e2c970a5e34f0ad680e196136b54
steadykingdev <steadykingdev@gmail.com>
2023-02-13 20:35:33 +0900

    feat: RestControllerAdvice로 exceptionHandling

---
+++ src/main/java/com/study/board/exception/CommonExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.study.board.exception;
+
+import com.study.board.dto.CommonResponseFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CommonExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonResponseFormat> handleValidationException(BindingResult bindingResult) {
+        return new ResponseEntity<>(CommonResponseFormat.createFail(bindingResult), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<CommonResponseFormat> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(CommonResponseFormat.createError(ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+}
