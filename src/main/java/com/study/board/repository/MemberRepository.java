ddd1d3eb53988e5dc2aafae6f411c15d2f6194a3
steadykingdev <steadykingdev@gmail.com>
2023-02-14 20:06:15 +0900

    fix: Optional import 삭제

--- src/main/java/com/study/board/repository/MemberRepository.java
+++ src/main/java/com/study/board/repository/MemberRepository.java
@@ -3,8 +3,6 @@ package com.study.board.repository;
 import com.study.board.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     boolean existsByLoginId(String loginId);
